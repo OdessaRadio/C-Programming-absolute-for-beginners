25a7908942fd97cd3edd3ae31007a571d47a1ce3
OdessaRadio <alexander_lyashko@hotmail.com>
2020-09-14 16:41:37 +0300

    Pointer to pointer practice

--- Variables/Variables practice/main.c
+++ Variables/Variables practice/main.c
@@ -6,16 +6,23 @@ int main ()
     int var2 = 10;
     int *pointer; // pointer variable declaration
 
-    int *pnt;
-    pnt = &var2;
+    int *pnt1;
+    pnt1 = &var2;
+
+    int *pnt2;
+    pnt2 = &pnt1;
+
+    printf("The address of the pointer variable pnt2 is: %x\n", &pnt2); // taking address of the pointer variable
+    printf("The value of the pointer variable pnt2 is: %x\n", pnt2); // taking address of the pointer variable
+    printf("The value of the actual variable by address in pnt2 is: %x\n\n", *pnt2); // taking address of the pointer variable
 
     // * - read the variable value by pointer address
     // & - take the address of the actual variable and store it in the pointer variable as address
 
-    printf("The address of the var2 variable is: %x\n", pnt);
-    printf("The value of the var2 is: %d\n", *pnt);
-    printf("The address of the pointer variable pnt is: %x\n", &pnt); // taking address of the pointer variable
-    printf("The value of the pointer variable pnt is: %x\n\n", pnt); // taking address of the pointer variable
+    printf("The address of the var2 variable is: %x\n", pnt1);
+    printf("The value of the var2 is: %d\n", *pnt1);
+    printf("The address of the pointer variable pnt is: %x\n", &pnt1); // taking address of the pointer variable
+    printf("The value of the pointer variable pnt is: %x\n\n", pnt1); // taking address of the pointer variable
 
     pointer = &var1; // taking address of the var variable and storing it in pointer variable
 
